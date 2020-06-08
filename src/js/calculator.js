b5c467f0f06db80b630853bfffd63604477f88a3
rajchandra3 <rajchandra963@gmail.com>
2020-06-08 13:57:19 +0530

    add service worker and manifest.json

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -330,3 +330,16 @@ $('.alert').hide();
 //         $('#submit').removeAttr('disabled');
 //     });
 // });
+
+// register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', ()=> {
+        navigator.serviceWorker.register('/serviceWorker.js').then((registration)=> {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, (err)=> {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
