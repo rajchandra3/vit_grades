a1d6ad308d5d634aae7b95b9ba6a1d71ab889ca7
rajchandra3 <rajchandra963@gmail.com>
2020-06-21 11:31:53 +0530

    add install button

--- serviceWorker.js
+++ serviceWorker.js
@@ -19,116 +19,136 @@ const isLocalhost = Boolean(
       )
   );
   
-  const PUBLIC_URL = isLocalhost?'http://localhost:5502':'https://getcgpa.rajchandra.me';
-  
-  function register(config) {
-    if (NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-      // The URL constructor is available in all browsers that support SW.
-      const publicUrl = new URL(PUBLIC_URL, this.location.href);
-      if (publicUrl.origin !== this.location.origin) {
-        // Our service worker won't work if PUBLIC_URL is on a different origin
-        // from what our page is served on. This might happen if a CDN is used to
-        // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-        return;
-      }
-  
-      this.addEventListener('load', () => {
-        const swUrl = `${PUBLIC_URL}/serviceWorker.js`;
-  
-        if (isLocalhost) {
-          // This is running on localhost. Let's check if a service worker still exists or not.
-          checkValidServiceWorker(swUrl, config);
-  
-          // Add some additional logging to localhost, pointing developers to the
-          // service worker/PWA documentation.
-          navigator.serviceWorker.ready.then(() => {
-            console.log('This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA');
-          });
-        } else {
-          // Is not localhost. Just register service worker
-          registerValidSW(swUrl, config);
-        }
-      });
-    }
-  }
-  
-  function registerValidSW(swUrl, config) {
-    navigator.serviceWorker
-      .register(swUrl)
-      .then(registration => {
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          if (installingWorker == null) {
-            return;
-          }
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === 'installed') {
-              if (navigator.serviceWorker.controller) {
-                // At this point, the updated precached content has been fetched,
-                // but the previous service worker will still serve the older
-                // content until all client tabs are closed.
-                alert(`📱New App version is here 🎉 \n🌟We have updated the app in the background\n✅Please close all the tabs for this page to view the updated version!\n🤖Updating the app will give you access to new features and security fixes`);
+const PUBLIC_URL = isLocalhost?'http://localhost:5502':'https://getcgpa.rajchandra.me';
 
-                // Execute callback
-                if (config && config.onUpdate) {
-                  config.onUpdate(registration);
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                if (installingWorker == null) {
+                    return;
                 }
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a
-                // "Content is cached for offline use." message.
-                console.log('Content is cached for offline use.');
-  
-                // Execute callback
-                if (config && config.onSuccess) {
-                  config.onSuccess(registration);
-                }
-              }
-            }
-          };
-        };
-      })
-      .catch(error => {
-        console.error('Error during service worker registration:', error);
-      });
-  }
-  
-  function checkValidServiceWorker(swUrl, config) {
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            // At this point, the updated precached content has been fetched,
+                            // but the previous service worker will still serve the older
+                            // content until all client tabs are closed.
+                            alert(`📱App version ${version} is here 🎉
+                                    \n🌟We have updated the app in the background
+                                    \n✅Please close all the tabs for this page to view the updated version!
+                                    \n🤖Updating the app will prevent data loss and security risks`);
+
+                            window.mixpanel.track('App New Version Prompted', {
+                                version,
+                            });
+                            // Execute callback
+                            if (config && config.onUpdate) {
+                                config.onUpdate(registration);
+                            }
+                        } else {
+                            // At this point, everything has been precached.
+                            // It's the perfect time to display a
+                            // "Content is cached for offline use." message.
+                            console.log('Content is cached for offline use.');
+
+                            // Execute callback
+                            if (config && config.onSuccess) {
+                                config.onSuccess(registration);
+                            }
+                        }
+                    }
+                };
+            };
+        })
+        .catch((error) => {
+            window.mixpanel.track('Error during service worker registration', {
+                version,
+            });
+            console.error('Error during service worker registration:', error);
+        });
+}
+
+function checkValidServiceWorker(swUrl, config) {
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl, {
-      headers: { 'Service-Worker': 'script' },
+        headers: { 'Service-Worker': 'script' },
     })
-      .then(response => {
-        // Ensure service worker exists, and that we really are getting a JS file.
-        const contentType = response.headers.get('content-type');
-        if (
-          response.status === 404 ||
-          (contentType != null && contentType.indexOf('javascript') === -1)
-        ) {
-          // No service worker found. Probably a different app. Reload the page.
-          navigator.serviceWorker.ready.then(registration => {
-            registration.unregister().then(() => {
-              this.location.reload();
-            });
-          });
-        } else {
-          // Service worker found. Proceed as normal.
-          registerValidSW(swUrl, config);
-        }
-      })
-      .catch(() => {
-        alert('No internet connection found. App is running in offline mode.');
-      });
-  }
-  
-  function unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready
-        .then(registration => {
-          registration.unregister();
+        .then((response) => {
+            // Ensure service worker exists, and that we really are getting a JS file.
+            const contentType = response.headers.get('content-type');
+            if (
+                response.status === 404 ||
+                (contentType != null &&
+                    contentType.indexOf('javascript') === -1)
+            ) {
+                // No service worker found. Probably a different app. Reload the page.
+                navigator.serviceWorker.ready.then((registration) => {
+                    registration.unregister().then(() => {
+                        window.location.reload();
+                    });
+                });
+            } else {
+                // Service worker found. Proceed as normal.
+                registerValidSW(swUrl, config);
+            }
         })
-        .catch(error => {
-          console.error(error.message);
+        .catch(() => {
+            alert(
+                'No internet connection found. App is running in offline mode.'
+            );
+        });
+}
+
+function register(config) {
+    if (!isLocalhost && 'serviceWorker' in navigator) {
+        // The URL constructor is available in all browsers that support SW.
+        const publicUrl = new URL(PUBLIC_URL, window.location.href);
+        if (publicUrl.origin !== window.location.origin) {
+            // Our service worker won't work if PUBLIC_URL is on a different origin
+            // from what our page is served on. This might happen if a CDN is used to
+            // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+            return;
+        }
+
+        window.addEventListener('load', () => {
+            const swUrl = `${PUBLIC_URL}/serviceWorker.js`;
+
+            if (isLocalhost) {
+                // This is running on localhost. Let's check if a service worker still exists or not.
+                checkValidServiceWorker(swUrl, config);
+
+                // Add some additional logging to localhost, pointing developers to the
+                // service worker/PWA documentation.
+                navigator.serviceWorker.ready.then(() => {
+                    console.log(
+                        'This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA'
+                    );
+                });
+            } else {
+                // Is not localhost. Just register service worker
+                registerValidSW(swUrl, config);
+            }
         });
     }
-  }
+}
+
+function unregister() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready
+            .then((registration) => {
+                registration.unregister();
+            })
+            .catch((error) => {
+                window.mixpanel.track('Error in Service worker', {
+                    version,
+                    error,
+                });
+                console.error(error.message);
+            });
+    }
+}
+
+register();
---
+++ src/js/installPWA.js
@@ -0,0 +1,90 @@
+const installBtn = document.querySelector('.install-app-button');
+console.log('mixpanel', mixpanel.track('pwa page loaded'))
+const hideContainer = () => {
+    document
+        .querySelector('.install-app-container')
+        .classList.toggle('hidden', true);
+};
+
+const showContainer = () => {
+    document
+        .querySelector('.install-app-container')
+        .classList.toggle('hidden', false);
+};
+
+window.addEventListener('load', (e) => {
+    console.log('page loaded');
+    window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('👍', 'beforeinstallprompt', event);
+        // Prevent the mini-infobar from appearing on mobile
+        event.preventDefault();
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = event;
+        // Remove the 'hidden' class from the install button container
+        showContainer();
+    });
+
+    window.addEventListener('appinstalled', (event) => {
+        console.log('👍', 'appinstalled', event);
+        mixpanel.track('App Installed');
+    });
+
+    // Track how the PWA was launched #
+    window.addEventListener('DOMContentLoaded', () => {
+        let displayMode = 'browser tab';
+        if (navigator.standalone) {
+            displayMode = 'standalone-ios';
+        }
+        if (window.matchMedia('(display-mode: standalone)').matches) {
+            displayMode = 'standalone';
+        }
+        // Log launch display mode to analytics
+        mixpanel.track('App Lauched', { mode: displayMode });
+        console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+    });
+
+    // Track when the display mode changes
+    window.addEventListener('DOMContentLoaded', () => {
+        window.matchMedia('(display-mode: standalone)').addListener((evt) => {
+            let displayMode = 'browser tab';
+            if (evt.matches) {
+                displayMode = 'standalone';
+            }
+            // Log display mode change to analytics
+            mixpanel.track('Display mode changed', {
+                mode: displayMode
+            });
+            console.log('DISPLAY_MODE_CHANGED', displayMode);
+        });
+    });
+});
+
+installBtn.addEventListener('click',(e) => {
+    mixpanel.track('Click: Install App button');
+    console.log('Clicked install app button');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        const reason = `The deferred prompt isn't available.`;
+        console.log(reason);
+        mixpanel.track('App Installation Failed', { reason });
+        hideContainer();
+        return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    promptEvent.userChoice.then((result) => {
+        console.log('👍', 'userChoice', result);
+        if (result.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+            mixpanel.track('Accepted: App Install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+            mixpanel.track('Declined: App Install prompt');
+        }
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+    });
+});
