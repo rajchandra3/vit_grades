b8ef928a5ff2bc25c3039d63715a2f9e8c1e4d2a
rajchandra3 <rajchandra963@gmail.com>
2020-10-02 00:37:37 +0530

    add new UI to internal color game ads

--- src/js/ads.js
+++ src/js/ads.js
@@ -55,37 +55,33 @@
 let getAds = ()=>{ //typeOfAd
     let ads = [
         {
-            type : `g`,
-            body : `Check coronavirus updates`,
-            link : `https://rajchandra.me/r/PoPmqT9pi?s=card_cta_ad&m=cgpa-calculator`,
-            linkText : `Check Now`
-        },
-        {
-            type : `e`,
-            body : `Stay Home, Save Lives!`,
-            link : `https://rajchandra.me/r/PoPmqT9pi?s=card_cta_ad&m=cgpa-calculator`,
-            linkText : `View Now`
-        },
-        {
             type : `tw`,
-            body : `Stay Indoor, Play Color Game`,
+            title: `Color Tile`,
+            logo: `https://color-tile.rajchandra.me/src/images/icons/icon-512x512.png`,
+            body : `Stay Indoor, Play Color Tile`,
             link : `https://rajchandra.me/r/0biYgwkl4?s=card_cta_ad&m=cgpa-calculator`,
             linkText : `Play Now`
         },
         {
             type : `tf`,
+            title: `Color Tile`,
+            logo: `https://color-tile.rajchandra.me/src/images/icons/icon-512x512.png`,
             body : `I challenge you to win 5 games in a row, can you?`,
             link : `https://rajchandra.me/r/0biYgwkl4?s=card_cta_ad&m=cgpa-calculator`,
             linkText : `Challenge Accepted?`
         },
         {
             type : `ts`,
+            title: `Color Tile`,
+            logo: `https://color-tile.rajchandra.me/src/images/icons/icon-512x512.png`,
             body : `Aren't you bored yet? Huh, I am, I play color tile do you?`,
             link : `https://rajchandra.me/r/0biYgwkl4?s=card_cta_ad&m=cgpa-calculator`,
             linkText : `Play Color Tile`
         },
         {
             type : `i`,
+            title: `Color Tile`,
+            logo: `https://color-tile.rajchandra.me/src/images/icons/icon-512x512.png`,
             body : `Checking my cgpa all the time is no fun, you know what is fun, THE COLOR GAME`,
             link : `https://rajchandra.me/r/0biYgwkl4?s=card_cta_ad&m=cgpa-calculator`,
             linkText : `Let's play`
@@ -95,24 +91,37 @@ let getAds = ()=>{ //typeOfAd
     let AdNumber = Math.floor(Math.random()*(ads.length));
     ad = ads[AdNumber];
     let templates = [
-            `<div class="custom-ads" style="
-                margin : 15px;
+            `
+            <div class="custom-ads" style="
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+                width: 100%;
+                margin : 0;
                 border-radius : 5px;
-                border : 0.5px solid #007bff;
-                padding : 8px 8px 8px 8px;
-                text-align : center;">
-                <p style="
-                    font-size : 10;
-                    font-style: monospace;
-                    font-weight : normal;">${ad.body}</p>
-                    <a
-                        class="btn btn-success btn-lg badge-pill badge-success"
-                        href="${ad.link}"
-                        target="_blank"
-                        role="button">
-                        ${ad.linkText}
-                    </a>
-            </div>`
+                border : 0.5px dashed #000;
+                text-align : left;"
+                >
+                <div class="d-flex">
+                    <div style="width: 25%;">
+                        <img width="100%" height="100%" src="${ad.logo}" alt="">
+                    </div>
+                    <div style="width: 75%; padding: 0.5rem;">
+                        <h4 class="font-weight-bolder">${ad.title}</h4>
+                        <span class="text-small text-secondary">${ad.body}</span>
+                        <a 
+                            href="${ad.link}"
+                            target="_blank"
+                            class="btn btn-secondary" 
+                            style="
+                                width: 100%; 
+                                margin-top: 1rem;
+                            "
+                        >
+                            ${ad.linkText}
+                        </a>
+                    </div>
+                </div>
+            </div>
+            `
     ]
     let templateNumber = Math.floor(Math.random()*(templates.length));
     return templates[templateNumber];
